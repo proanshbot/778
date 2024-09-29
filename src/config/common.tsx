998ecb6f23d2282a2c8e2c5581a88566de497a48
proanshbot <105037693+proanshbot@users.noreply.github.com>
2024-09-30 00:37:07 +0530

    Update common.tsx

--- src/config/common.tsx
+++ src/config/common.tsx
@@ -20,10 +20,10 @@ const BotIcon = createIcon({
 });
 
 export const config: AppConfig = {
-  name: 'Demo Bot',
+  name: 'ALCON',
   icon: BotIcon,
   inviteUrl:
-    'https://discord.com/api/oauth2/authorize?client_id=1070011901385375845&permissions=8&scope=bot',
+    'https://discord.com/oauth2/authorize?client_id=1289977955597946881&permissions=8&integration_type=0&scope=bot',
   guild: {
     //filter guilds that user has no permissions to manage it
     filter: (guild) => (Number(guild.permissions) & PermissionFlags.ADMINISTRATOR) !== 0,
